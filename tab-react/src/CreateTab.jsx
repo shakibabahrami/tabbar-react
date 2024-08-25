f6a431cb53576cfbff0399ff40be18b47fb86a7b
shakiba bahrami <shakibabahramimosaed@gmail.com>
2024-08-25 16:00:18 +0330

    final

--- tab-react/src/CreateTab.jsx
+++ tab-react/src/CreateTab.jsx
@@ -1,24 +1,9 @@
 import React from "react";
-import { useState } from "react";
 
 function CreateTab(props) {
   const setTabData = props.func;
-  //   console.log(selected);
-
-  const [selected, setSelected] = useState([true, false, false, false]);
-
-  const changeText = () => {
-    setTabData(props.data);
-    console.log(props.data);
-    
-    // setSelected(!selected);
-  };
-
-  return (
-    <>
-      <li onClick={changeText}>{props.data.name}</li>
-    </>
-  );
+  const changeText = () => setTabData(props.data);
+  return <li onClick={changeText}>{props.data.name}</li>;
 }
 
 export default CreateTab;
